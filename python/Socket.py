a5a5a89e7c5f42876b0cfa7b268639b66262e7f5
Homare3 <135232563+Homare3@users.noreply.github.com>
2024-04-30 17:22:35 +0900

    Delete python directory

--- python/Socket.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-class Socket:
-    def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-    def sendto(self, DATA, HOST = '127.0.0.1', PORT = 50007):
-        result = str(DATA)
-        self.client.sendto(result.encode('utf-8'),(HOST,PORT))
