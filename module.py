eeccc4559c54d68d1723720c230bffcb3fc08677
Homare3 <135232563+Homare3@users.noreply.github.com>
2024-09-24 22:23:41 +0900

    Update module.py

--- module.py
+++ module.py
@@ -32,6 +32,7 @@ class OCR:
                                           image_context={"language_hints": ["ja", "en"]})
         if response.text_annotations:
             text_annotations = response.full_text_annotation
+            st.write(text_annotations)
             sorted_annotations = self.sort_annotations(text_annotations[1:])
             result_list = [annotation.description for annotation in sorted_annotations]
         else:
